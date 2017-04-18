aabbabc4ae0588df8c23468968cecca362f4b0cc
Junghwan John Goh <jhgoh@cern.ch>
2017-04-18 14:55:03 +0200

    use DYJets sample

--- EGMTune/test/getHLTConfig.sh
+++ EGMTune/test/getHLTConfig.sh
@@ -2,7 +2,7 @@
 
 MENU=/users/sharper/2017/egamma/exampleMenus/egPMv1/V4
 GLOBALTAG=90X_upgrade2017_TSG_Hcal_V2
-INPUT=/store/mc/PhaseIFall16DR/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/GEN-SIM-RAW/FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v2/130000/BE521173-FD10-E711-A3FE-02163E0176C2.root
+INPUT=/store/mc/PhaseIFall16DR/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/GEN-SIM-RAW/FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v1/130001/FE64C02E-A70E-E711-B368-FA163E1C071E.root
 PROCESS=HLT2
 #HLTPATH=HLTriggerFirstPath,HLT_PFHT400_SixJet30_DoubleBTagCSV_p056_v6,HLTriggerFinalPath 
 

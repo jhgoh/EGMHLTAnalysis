67acf06eac1629ae1664ceae98b0b47e5669212d
Junghwan John Goh <jhgoh@cern.ch>
2017-04-18 08:42:05 +0200

    include all paths

--- EGMTune/test/getHLTConfig.sh
+++ EGMTune/test/getHLTConfig.sh
@@ -4,11 +4,11 @@ MENU=/users/sharper/2017/egamma/exampleMenus/egPMv1/V4
 GLOBALTAG=90X_upgrade2017_TSG_Hcal_V2
 INPUT=/store/mc/PhaseIFall16DR/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/GEN-SIM-RAW/FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v2/130000/BE521173-FD10-E711-A3FE-02163E0176C2.root
 PROCESS=HLT2
-HLTPATH=HLTriggerFirstPath,HLT_PFHT400_SixJet30_DoubleBTagCSV_p056_v6,HLTriggerFinalPath 
+#HLTPATH=HLTriggerFirstPath,HLT_PFHT400_SixJet30_DoubleBTagCSV_p056_v6,HLTriggerFinalPath 
 
 hltGetConfiguration $MENU --globaltag $GLOBALTAG --input $INPUT \
     --setup  /dev/CMSSW_9_0_1/HLT  \
     --mc --process $PROCESS --full --offline --l1-emulator FullSimHcalTP \
-    --unprescale --max-events 100 --output full \
-    --path $HLTPATH > hlt_cfg.py
+    --unprescale --max-events 100 --output full > hlt_cfg.py
+    #--path $HLTPATH > hlt_cfg.py
 
